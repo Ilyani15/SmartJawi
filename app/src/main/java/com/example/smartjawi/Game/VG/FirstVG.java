9f0106d077c723d450b4605b558191494fedc2a6
aqey <aqilkamel49@gmail.com>
2024-01-09 02:35:23 +0800

    vg 2

--- app/src/main/java/com/example/smartjawi/Game/VG/FirstVG.java
+++ app/src/main/java/com/example/smartjawi/Game/VG/FirstVG.java
@@ -68,7 +68,7 @@ public class FirstVG extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FirstVG.this, DD_Game_Nombor1.class);
+                Intent intent = new Intent(FirstVG.this, BetulSalahNombor1.class);
                 startActivity(intent);
             }
         });
